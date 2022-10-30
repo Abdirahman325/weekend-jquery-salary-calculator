669a13b37ad24c9f375651c3302f0d996ce56fc4
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-10-30 17:21:32 -0500

    got it up and working

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(onReady);
 function onReady() {
     console.log ('ready to start'); }
 
-            $(document).on('click', '#sumbitButton', addEmployee);
+            $(document).on('click', '#submitButton', addEmployee);
             $(document).on('click', '.deleteButton', onDelete);
     
 
@@ -34,6 +34,54 @@ employees.push(newEmployees);
 
 
 
+ //Seeing if i need a function before the append
+ $('#employeeList').append(`
+ <tr>
+    <td>${firstNameInputs}</td>
+    <td>${lastNameInputs}</td>
+    <td>${employeeIDNumberInputs}</td>
+    <td>${jobTitleInputs}</td>
+    <td>${annualSalaryInputs}</td>
+    <td>
+    <button class="deleteButton">Delete</button>
+    </td>
+ </tr>`);
+
+
+ //emptying the array
+ $("#firstNameInputs").val('');
+ $("#lastNameInputs").val('');
+ $("#idNumberInputs").val('');
+ $("#jobTitleInputs").val('');
+ $("#annualSalaryInputs").val('');
+
+
+monthlyTotal();
+
 
 }
+function onDelete() {
+    let tr = $(this).parents('tr');
+    tr.remove();
+    alert('Employee deleted ❌',employees);
+
+  }
+
+  function monthlyTotal() {
+    let total = 0;
+    $('.totalMonthlyCost').empty();
+    for (let salary of employees){
+        total += Number(salary.annualSalary);
+    }
+    $('.totalMonthlyCost').append(`Total Monthly Cost: ${total / 12}`);
+
+    if((total/12) >= 20000){
+        $('.totalMonthlyCost').css('background-color', 'red');
+    }
+  }
+
+
+
+
+
 
