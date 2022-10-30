2b511947dafa2a4f800b6efde140a05d53cbc862
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-10-30 15:14:45 -0500

    grinding

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ $(document).ready(onReady);
 
 
 function onReady() {
-    console.log ('ready to start');{
+    console.log ('ready to start'); }
 
             $(document).on('click', '#sumbitButton', addEmployee);
             $(document).on('click', '.deleteButton', onDelete);
-    }
+    
+
+
+    let employee =[];
+
+function addEmployee() {
+
+let firstNameInputs = $('#firstNameInputs').val();
+let lastNameInputs = $('#lastNameInputs').val();
+let employeeIDNumberInputs = $('#idNumberInputs').val();
+let jobTitleInputs = $('#jobTitleInputs').val();
+let annualSalaryInputs = $('#annualSalaryInputs').val();
+
+
+
 }
+
